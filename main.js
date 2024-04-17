359a214458729f57ddc5dfa10b73c3ef57eb57bd
“[firstname <[gmugsk@gmail.com]>
2024-04-17 15:55:33 +0500

    Assignment 14

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Assignment 14
+/*Guest List: If you could invite anyone, living or deceased, to dinner,
+ who would you invite? Make a list that includes at least three people
+  you’d like to invite to dinner. Then use your list to print a message
+  to each person, inviting them to dinner.*/
+var guestlistArray = ["Abdul Rehman", "Saifur Rehman", "Hamood ur Rehman", "Ebad ur Rehman"];
+guestlistArray.map((items) => (console.log(`Dear ${items} , WE are pleased TO invite
+ you on a dinner today as a special guest, Thanks.`)));
---
+++ main.ts
@@ -0,0 +1,8 @@
+//Assignment 14
+/*Guest List: If you could invite anyone, living or deceased, to dinner,
+ who would you invite? Make a list that includes at least three people
+  you’d like to invite to dinner. Then use your list to print a message 
+  to each person, inviting them to dinner.*/
+  var guestlistArray: string []= ["Abdul Rehman", "Saifur Rehman", "Hamood ur Rehman","Ebad ur Rehman"];
+guestlistArray.map((items) => (console.log(`Dear ${items} , WE are pleased TO invite
+ you on a dinner today as a special guest, Thanks.`))); 
